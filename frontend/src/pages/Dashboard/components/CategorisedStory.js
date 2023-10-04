736cf185eb691cd9deafa6d6a4ce00300b31c082
mrinal4703 <mrinal.gupta@mitaoe.ac.in>
2023-10-04 08:35:04 +0530

    Categorised Story almost completed, slider is added, @Neeraj your work is to add the real time data, I will help as well, for time being, I am pushing it.

--- frontend/src/pages/Dashboard/components/CategorisedStory.js
+++ frontend/src/pages/Dashboard/components/CategorisedStory.js
@@ -78,73 +78,8 @@ const CategorisedStory = () => {
                 </div>
                 <div className={"w-4/5 max-md:w-11/12"}>
                     <TopBar/>
-                    {/*<p className={'text-2xl'}>{selectedType.number} {selectedType.id} {selectedType.type}</p>*/}
-                    {/*<ProneAreas />*/}
-
-                    {/*<div className="grid grid-cols-5 grid-rows-2">*/}
-                    {/*    /!* Top Left *!/*/}
-                    {/*    <div className="col-span-2 row-span-1 bg-purple_primary" style={{*/}
-                    {/*        display: 'flex',*/}
-                    {/*        flexDirection: 'column',*/}
-                    {/*        justifyContent: 'flex-end',*/}
-                    {/*        position: 'relative'*/}
-                    {/*    }}>*/}
-                    {/*        /!*<img src={storywave2} className='w-full h-48' alt=''*!/*/}
-                    {/*        /!*     style={{position: 'absolute', top: 0, left: 0}}/>*!/*/}
-                    {/*        <div className={'w-24 my-4 h-32 mx-2 z-10'} style={{position: 'absolute', top: 0, left: 0}}>*/}
-                    {/*            <p className='z-10 text-center text-sm'>Story No.</p>*/}
-                    {/*            <p className='z-10 my-2 text-center text-l'>{selectedStory.id}</p>*/}
-                    {/*        </div>*/}
-                    {/*        <img src={bookmark} className='w-24 h-32 mx-2 z-3' alt=''*/}
-                    {/*             style={{position: 'absolute', top: 0, left: 0}}/>*/}
-                    {/*        <div className="mx-2 mr-4 mb-5 font-sans text-4xl text-white font-black"*/}
-                    {/*             style={{alignSelf: 'flex-end'}}>*/}
-                    {/*            {selectedStory.title}*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-
-
-                    {/*    /!* Top Right *!/*/}
-                    {/*    <div className="col-span-3 row-span-1 bg-green-500 brightness-75" style={{*/}
-                    {/*        position: 'relative',*/}
-                    {/*        background: `url(${selectedStory.mainimage})`,*/}
-                    {/*        backgroundSize: 'cover',*/}
-                    {/*        backgroundPosition: 'center',*/}
-                    {/*        backgroundRepeat: 'no-repeat',*/}
-                    {/*    }}>*/}
-                    {/*    </div>*/}
-
-                    {/*    <div className="col-span-2 row-span-1">*/}
-                    {/*        <div className="mx-auto my-auto h-full flex items-center justify-center flex-col">*/}
-                    {/*            <div className="flex my-2 items-center">*/}
-                    {/*                <CiCalendarDate className={'text-xl'}/>*/}
-                    {/*                <p className="mx-2 text-center">{selectedStory.date}</p>*/}
-                    {/*            </div>*/}
-
-                    {/*            <div className="flex my-2 items-center">*/}
-                    {/*                <IoLocationOutline className={'text-xl'}/>*/}
-                    {/*                <p className="mx-2 text-center">{selectedStory.place}</p>*/}
-                    {/*            </div>*/}
-
-                    {/*            <div className="flex my-2 items-center">*/}
-                    {/*                <TbAlertTriangle className={'text-l'}/>*/}
-                    {/*                <p className="mx-2 text-center">{selectedStory.type}</p>*/}
-                    {/*            </div>*/}
-
-                    {/*            <div className="flex my-2 items-center">*/}
-                    {/*                <IoPersonOutline className={'text-l'}/>*/}
-                    {/*                <p className="mx-2 text-center">{selectedStory.author}</p>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-
-                    {/*    </div>*/}
-
-                    {/*    /!* Bottom Right *!/*/}
-                    {/*    <div className="col-span-3 row-span-1">*/}
-                    {/*        <p className={'m-4 mr-6 text-end'}>{selectedStory.para1}</p>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<p className={'mr-6 mx-3 my-2 text-start'}>{selectedStory.para2}</p>*/}
+                    <p className={'mx-2 my-2 mb-5 font-sans text-start text-3xl font-black'}>Tragic {storyType} Stories
+                        in India</p>
 
                     <p className={'text-2xl'}>{selectedStory1.title} {selectedStory1.type}</p>
 
@@ -157,26 +92,86 @@ const CategorisedStory = () => {
                         <div id='slider'
                              className='w-screen h-[250px] overflow-hidden whitespace-nowrap scroll-smooth scrollbar-hide'>
                             {/* Duplicate the slides to create a loop */}
+
+                            {/*{selectedStory1 && selectedStory1.map((item, index) => (*/}
+                            {/*    <Link to={`/Story/${item.id}`}>*/}
+                            {/*        <img*/}
+                            {/*            key={index}*/}
+                            {/*            className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
+                            {/*            src={item.mainimage}*/}
+                            {/*            alt='/'*/}
+                            {/*        />*/}
+                            {/*        <div className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-0'>*/}
+                            {/*            <div className='flex-col justify-between text-white w-full p-2 mt-auto mb1'>*/}
+                            {/*                <div className='font-bold mb-1 flex justify-between items-end'>*/}
+                            {/*                    <h2 className='text-start mx-2 text-m'>{item.title}</h2>*/}
+                            {/*                </div>*/}
+                            {/*                <h3 className='text-end text-sm'>{item.date}</h3>*/}
+                            {/*            </div>*/}
+                            {/*        </div>*/}
+                            {/*    </Link>*/}
+                            {/*))}*/}
+
                             {selectedStory1 && selectedStory1.map((item, index) => (
-                                <Link to={`/Story/${item.id}`}>
+                                <Link to={`/Story/${item.id}`} key={index} className='relative'>
                                     <img
-                                        key={index}
-                                        className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'
+                                        className='w-[250px] inline-block brightness-50 mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'
                                         src={item.mainimage}
                                         alt='/'
                                     />
+                                    <div
+                                        className='absolute bottom-0 left-0 right-0 mx-2 bg-black bg-opacity-0 flex flex-col justify-end'>
+                                        <div className='text-white w-full p-2'>
+                                            <div className='font-bold mb-1 flex justify-between items-end'>
+                                                <h2 className='text-start mx-2 text-m'>{item.title}</h2>
+                                            </div>
+                                            <h3 className='text-end text-sm'>{item.date}</h3>
+                                        </div>
+                                    </div>
                                 </Link>
                             ))}
+
                             {selectedStory1 && selectedStory1.map((item, index) => (
-                                <Link to={`/Story/${item.id}`}>
+                                <Link to={`/Story/${item.id}`} key={index + (selectedStory1.length || 0)}
+                                      className='relative'>
                                     <img
-                                        key={index + (selectedStory1.length || 0)}
-                                        className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'
+                                        className='w-[250px] inline-block brightness-50 mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'
                                         src={item.mainimage}
                                         alt='/'
                                     />
+                                    <div
+                                        className='absolute bottom-0 left-0 right-0 mx-2 bg-black bg-opacity-0 flex flex-col justify-end'>
+                                        <div className='text-white w-full p-2'>
+                                            <div className='font-bold mb-1 flex justify-between items-end'>
+                                                <h2 className='text-start mx-2 text-m'>{item.title}</h2>
+                                            </div>
+                                            <h3 className='text-end text-sm'>{item.date}</h3>
+                                        </div>
+                                    </div>
                                 </Link>
                             ))}
+
+
+                            {/*{selectedStory1 && selectedStory1.map((item, index) => (*/}
+                            {/*    <Link to={`/Story/${item.id}`}>*/}
+                            {/*        <img*/}
+                            {/*            key={index + (selectedStory1.length || 0)}*/}
+                            {/*            className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
+                            {/*            src={item.mainimage}*/}
+                            {/*            alt='/'*/}
+                            {/*        />*/}
+                            {/*        <div className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-0'>*/}
+                            {/*            <div className='flex-col justify-between text-white w-full p-2 mt-auto mb1'>*/}
+                            {/*                <div className='font-bold mb-1 flex justify-between items-end'>*/}
+                            {/*                    <h2 className='text-start mx-2 text-m'>{item.title}</h2>*/}
+                            {/*                </div>*/}
+                            {/*                <h3 className='text-end text-sm'>{item.date}</h3>*/}
+                            {/*            </div>*/}
+                            {/*        </div>*/}
+                            {/*    </Link>*/}
+                            {/*))}*/}
+
+
                         </div>
                         <MdChevronRight
                             className='opacity-50 cursor-pointer hover:opacity-100 bg-stone-300 rounded-2xl '
@@ -184,83 +179,6 @@ const CategorisedStory = () => {
                             size={40}
                         />
                     </div>
-
-                    {/*<div className='relative my-2 flex gap-2 items-center '>
-                        <MdChevronLeft
-                            className='opacity-50 cursor-pointer hover:opacity-100 bg-stone-300 rounded-2xl '
-                            onClick={slideLeft}
-                            size={40}
-                        />*/}
-
-                        {/*<div id='slider'*/}
-                        {/*     className='w-screen h-[250px] overflow-hidden whitespace-nowrap scroll-smooth scrollbar-hide'>*/}
-                        {/*    /!* Duplicate the slides to create a loop *!/*/}
-                        {/*    {selectedStory1.mainimage && selectedStory1.mainimage.map((item, index) => (*/}
-                        {/*        <Link to={`/Story/${item.id}`}>*/}
-                        {/*            <img*/}
-                        {/*                key={index}*/}
-                        {/*                className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
-                        {/*                src={item}*/}
-                        {/*                alt='/'*/}
-                        {/*            />*/}
-                        {/*        </Link>*/}
-                        {/*    ))}*/}
-                        {/*    {selectedStory1.mainimage && selectedStory1.mainimage.map((item, index) => (*/}
-                        {/*        <Link to={`/Story/${item.id}`}>*/}
-                        {/*            <img*/}
-                        {/*                key={index + (selectedStory1.length || 0)}*/}
-                        {/*                className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
-                        {/*                src={item}*/}
-                        {/*                alt='/'*/}
-                        {/*            />*/}
-                        {/*        </Link>*/}
-                        {/*    ))}*/}
-                        {/*</div>*/}
-
-                        {/*<div id='slider'*/}
-                        {/*     className='w-screen h-[250px] overflow-hidden whitespace-nowrap scroll-smooth scrollbar-hide'>*/}
-                        {/*    /!* Duplicate the slides to create a loop *!/*/}
-                        {/*    {selectedStory1 && selectedStory1.map((item, index) => (*/}
-                        {/*        <Link key={index} to={`/Story/${item.id}`}>*/}
-                        {/*            <img*/}
-                        {/*                className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
-                        {/*                src={item.mainimage}*/}
-                        {/*                alt='/'*/}
-                        {/*            />*/}
-                        {/*        </Link>*/}
-                        {/*    ))}*/}
-                        {/*    {selectedStory1 && selectedStory1.map((item, index) => (*/}
-                        {/*        <Link key={index + (selectedStory1.length || 0)} to={`/Story/${item.id}`}>*/}
-                        {/*            <img*/}
-                        {/*                className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
-                        {/*                src={item.mainimage}*/}
-                        {/*                alt='/'*/}
-                        {/*            />*/}
-                        {/*        </Link>*/}
-                        {/*    ))}*/}
-                        {/*</div>*/}
-
-                        {/*<div id='slider'*/}
-                        {/*     className='w-screen h-[250px] overflow-hidden whitespace-nowrap scroll-smooth scrollbar-hide'>*/}
-                        {/*    /!* Duplicate the slides to create a loop *!/*/}
-                        {/*    {selectedStory1.map((item, index) => (*/}
-                        {/*        <Link key={index} to={`/Story/${item.id}`}>*/}
-                        {/*            <img*/}
-                        {/*                className='w-[250px] inline-block mx-1 my-auto p-2 cursor-pointer rounded-2xl hover:scale-105 ease-in-out duration-300'*/}
-                        {/*                src={item.mainimage}  // Assuming mainimage is the correct property*/}
-                        {/*                alt='/'*/}
-                        {/*            />*/}
-                        {/*        </Link>*/}
-                        {/*    ))}*/}
-                        {/*</div>*/}
-
-                    {/*    <MdChevronRight*/}
-                    {/*        className='opacity-50 cursor-pointer hover:opacity-100 bg-stone-300 rounded-2xl '*/}
-                    {/*        onClick={slideRight}*/}
-                    {/*        size={40}*/}
-                    {/*    />*/}
-                    {/*</div>*/}
-
                 </div>
             </div>
         </div>
