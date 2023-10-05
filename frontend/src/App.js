97b32103a5b75d8e6d184e6761f88a31430c3d18
priyanshikotian0902 <priyanshi.kotian@mitaoe.ac.in>
2023-10-05 09:53:55 +0530

    Addition of map and some modifications are remaining

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Landing from "./pages/Landing/Landing";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Story from "./pages/Dashboard/components/Story";
 import CategorisedStory from "./pages/Dashboard/components/CategorisedStory";
+import Help from './pages/Help/Help';
+
+
 
 function App() {
     return (
@@ -15,6 +18,7 @@ function App() {
                     <Route exact path="/dashboard" element={<Dashboard/>}/>
                     <Route exact path="/Story/:storyId" element={<Story/>}/>
                     <Route exact path="/CategorisedStory/:storyId" element={<CategorisedStory/>}/>
+                    <Route exact path="/Help" element={<Help/>}/>
                 </Routes>
             </Router>
         </div>
