00b881d55d9a36421dc8576bffb668857caf6bae
priyanshikotian0902 <131985058+priyanshikotian0902@users.noreply.github.com>
2023-10-05 11:36:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,9 @@ import Landing from "./pages/Landing/Landing";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Story from "./pages/Dashboard/components/Story";
 import CategorisedStory from "./pages/Dashboard/components/CategorisedStory";
-<<<<<<< HEAD
 import Help from './pages/Help/Help';
-
-
-=======
 import Signup from "./pages/Signup/Signup"
 import Login from "./pages/Login/Login"
->>>>>>> 2b5d0d9a16a23758ffd1c97ce6ad5dac91b4b8cc
 
 function App() {
     return (
@@ -22,14 +17,12 @@ function App() {
                     <Route exact path="/" element={<Landing/>}/>
                     <Route exact path="/dashboard" element={<Dashboard/>}/>
                     <Route exact path="/Story/:storyId" element={<Story/>}/>
-<<<<<<< HEAD
                     <Route exact path="/CategorisedStory/:storyId" element={<CategorisedStory/>}/>
                     <Route exact path="/Help" element={<Help/>}/>
 =======
                     <Route exact path="/CategorisedStory/:storyType" element={<CategorisedStory/>}/>
                     <Route exact path="/login" element={<Login/>}></Route>
                     <Route exact path="/signup" element={<Signup/>}></Route>
->>>>>>> 2b5d0d9a16a23758ffd1c97ce6ad5dac91b4b8cc
                 </Routes>
             </Router>
         </div>
