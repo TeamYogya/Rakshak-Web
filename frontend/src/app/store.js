229afd1da4d824bb2d5a0ff922b96146ac71e8e9
ABRAHAM AHMED <120266994+21abraham@users.noreply.github.com>
2024-01-13 19:58:59 +0530

    Add abraham

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,16 +3,19 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { userAuthApi } from '../services/userAuthApi';
 import { storyInputApi } from '../services/storyInputApi';
 import { emergencyApi } from '../services/emergencyApi';
+import {geofenceApi} from "../services/geofenceApi";
 
 export const store = configureStore({
   reducer: {
     [userAuthApi.reducerPath]: userAuthApi.reducer,
     [storyInputApi.reducerPath]: storyInputApi.reducer,
+    [geofenceApi.reducerPath]: geofenceApi.reducer,
     [emergencyApi.reducerPath]: emergencyApi.reducer,  // Add this line
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(userAuthApi.middleware)
+      .concat(geofenceApi.middleware)
       .concat(storyInputApi.middleware)
       .concat(emergencyApi.middleware),  // Add this line
 });
